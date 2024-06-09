53167d9ac226c41adaabd295bd6196216ea89157
puzzl <puzzleuri@mail.ru>
2024-06-09 23:16:44 +0300

    удалил субмодули

---
+++ app/src/main/java/com/example/gitbaseforsubmodules/mywork/SubmoduleImpl.kt
@@ -0,0 +1,10 @@
+package com.example.gitbaseforsubmodules.mywork
+
+import app.GitSubmodule1.app.src.main.java.com.example.gitsubmodule1
+class SubmoduleImpl {
+
+    val submodule1: AlarmDialogFragment? = null
+
+
+
+}
